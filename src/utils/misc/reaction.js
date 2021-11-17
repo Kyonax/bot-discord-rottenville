5c4b5454cbbeb3ca51ee70305ca4668f44c28c19
Kyonax <kyonax25@gmail.com>
2021-11-17 17:34:18 -0500

    Fixing Emojis

--- src/utils/misc/reaction.js
+++ src/utils/misc/reaction.js
@@ -42,7 +42,7 @@ module.exports = {
         );
         //Polls
         const pollEmbed = message.embeds.find((msg) =>
-          msg.title.includes("Encuesta")
+          msg.title.includes("Poll")
         );
         //Embed Suggestion
         if (suggestionEmbed) {
