f993713b37487a10f3a11a96a7f399f95222da5b
Kyonax <kyonax25@gmail.com>
2022-01-10 18:39:16 -0500

    Level Up - Update

--- src/event/messages/message.js
+++ src/event/messages/message.js
@@ -258,11 +258,11 @@ module.exports = class MessageEvent extends BaseEvent {
         if (newLevel > memberLevel) {
           const emojiLevelUp = synchronous.emojiID[0].levelup;
           const levelChannel = message.guild.channels.cache.find(
-            (ch) => ch.name === "🎄・level-up"
+            (ch) => ch.name === "👽・level-up"
           );
           if (!levelChannel) {
             return message.guild.channels
-              .create("🎄・level-up", {
+              .create("👽・level-up", {
                 type: "text",
                 permissionOverwrites: [
                   {
