877fb13feafe3087b1ecc9adcace2bfbf5069e63
Kyonax <kyonax25@gmail.com>
2022-03-31 18:43:25 -0500

    Updated Rewards

--- src/command/owner/battleRewards.js
+++ src/command/owner/battleRewards.js
@@ -78,7 +78,7 @@ module.exports = class BattlesCommand extends BaseCommand {
     );
 
     JSON.parse(_jsonString).forEach((_member) => {
-      if (_member.guildID == member.guild.id) {
+      if (_member.guildID == autor.guild.id) {
         if (message.author.id == _member.memberID) {
           ObjectAutor = _member;
         }
