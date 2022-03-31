cf3eb05f8fcfca18d82c107b8060a87337cf705d
Kyonax <kyonax25@gmail.com>
2022-03-31 07:58:57 -0500

    Alpha DAO Content

--- src/utils/misc/twitter.js
+++ src/utils/misc/twitter.js
@@ -38,6 +38,26 @@ var sol_reaper = process.env.TWITTER_SOLREAPER;
 var lilmayo = process.env.TWITTER_LILMAYO;
 var boogle = process.env.TWITTER_BOOGLE;
 var dizan = process.env.TWITTER_DIZAN;
+
+
+var rotten_dao = process.env.THE_ROTTENS_DAO
+var rotten_dao_kyonax =process.env.ROTTEN_DAO_KYONAX
+var rotten_dao_sam = process.env.ROTTEN_DAO_SAM
+var rotten_dao_rec = process.env.ROTTEN_DAO_REC
+var rotten_dao_bstingo = process.env.ROTTEN_DAO_BSTINGO
+var rotten_mrmike = process.env.ROTTEN_DAO_MRMIKE
+var rotten_dao_rojo = process.env.ROTTEN_DAO_ROJO
+var rotten_dao_brito = process.env.ROTTEN_DAO_BRITO
+var rotten_dao_sandy = process.env.ROTTEN_DAO_SANDYLU
+var rotten_dao_caracolero = process.env.ROTTEN_DAO_CARACOLERO
+var rotten_dao_baiden = process.env.ROTTEN_DAO_BAIDEN
+var rotten_dao_nyciiraf = process.env.ROTTEN_DAO_NYCIIRAF
+var rotten_dao_triumph = process.env.ROTTEN_DAO_TRIUMPH
+var rotten_dao_mrwilson = process.env.ROTTEN_DAO_MRWILSON
+var rotten_dao_sanin = process.env.ROTTEN_DAO_SANIN
+var rotten_dao_rysoo = process.env.ROTTEN_DAO_RYSOO
+
+
 //Crypto Twitter
 module.exports.twitter = async (bot) => {
   const emoji_afirmado = synchronous.emojiID[0].afirmado;
@@ -67,6 +87,21 @@ module.exports.twitter = async (bot) => {
       lilmayo,
       boogle,
       dizan,
+      rotten_dao,
+      rotten_dao_kyonax,
+      rotten_dao_sam,
+      rotten_dao_rec,
+      rotten_dao_bstingo,
+      rotten_dao_rojo,
+      rotten_dao_brito,
+      rotten_dao_sandy,
+      rotten_dao_caracolero,
+      rotten_dao_baiden,
+      rotten_dao_nyciiraf,
+      rotten_dao_triumph,
+      rotten_dao_mrwilson,
+      rotten_dao_sanin,
+      rotten_dao_rysoo
     ],
   });
 
@@ -94,8 +129,54 @@ module.exports.twitter = async (bot) => {
       sol_reaper,
       lilmayo,
       boogle,
-      dizan,
+      dizan
     ];
+
+    let rotten_DAOaccountTwitter = [      
+      rotten_dao,
+      rotten_dao_kyonax,
+      rotten_dao_sam,
+      rotten_dao_rec,
+      rotten_dao_bstingo,
+      rotten_dao_rojo,
+      rotten_dao_brito,
+      rotten_dao_sandy,
+      rotten_dao_caracolero,
+      rotten_dao_baiden,
+      rotten_dao_nyciiraf,
+      rotten_dao_triumph,
+      rotten_dao_mrwilson,
+      rotten_dao_sanin,
+      rotten_dao_rysoo
+    ];
+
+    rotten_DAOaccountTwitter.forEach((account) => {
+      if (tweet.user.id == account) {
+        var url_kyo_tweet =
+          "https://twitter.com/" +
+          tweet.user.screen_name +
+          "/status/" +
+          tweet.id_str;
+        try {
+          let channel = bot.channels
+            .fetch(process.env.DISCORD_CHANNEL_DAO_ALPHA)
+            .then((channel) => {
+              channel.send(
+                `${putEmoji(
+                  bot,
+                  emoji_afirmado
+                )} **Alpha Content from a <@&958140020517109781> Member <@&900354883708936202>**\n${url_kyo_tweet}`
+              );
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+
     accountTwitter.forEach((account) => {
       if (tweet.user.id == account) {
         var url_kyo_tweet =
@@ -122,6 +203,8 @@ module.exports.twitter = async (bot) => {
         }
       }
     });
+
+
 
     if (tweet.user.id == process.env.TWITTER_ROTTEN) {
       var url_kyo_tweet =
