51a38d6af7a0b215957f20c135318dfee4484da5
Kyonax <kyonax25@gmail.com>
2022-04-26 20:25:10 -0500

    locale: New Data upload FAQ
    FAQ questions and files for the command

--- src/command/guild/info.js
+++ src/command/guild/info.js
@@ -1,7 +1,7 @@
 //Importación especifica de Metodos - RichEmbed - Perms - cautioncolor Color
 const { MessageEmbed } = require("discord.js");
 const { noneColor } = require("../../../database/utils/color/color.json");
-const FAQ = require ("../../../database/conectors/faq.json")
+const FAQ = require("../../../database/conectors/faq.json");
 const { putEmoji, getMember } = require("../../utils/misc/functions");
 const { synchronous } = require("../../../database/utils/emojis/emojis.json");
 //Importación de el cuerpo de Comandos e importación de Conexión Base de Datos
@@ -9,15 +9,14 @@ const fs = require("fs");
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const BaseCommand = require("../../utils/structure/BaseCommand");
 
-function readTextFile(file)
-{
+function readTextFile(file) {
   try {
-    const data = fs.readFileSync(file, 'utf8');
+    const data = fs.readFileSync(file, "utf8");
     console.log(data);
-    return data
+    return data;
   } catch (err) {
     console.error(err);
-  }  
+  }
 }
 
 //Exportación del Comando news
@@ -43,11 +42,10 @@ module.exports = class InfoCommand extends BaseCommand {
     let cancelado = synchronous.emojiID[0].cancelado;
     let afirmado = synchronous.emojiID[0].afirmado;
     let textResponse = putEmoji(bot, cancelado) + " **Information not found.**";
-    if (contentArgs == "alpha") {
-      let embed = new MessageEmbed().setTitle(FAQ["alpha"].tittle).setDescription(readTextFile(FAQ["alpha"].description))
-      message.channel.send(embed)
-    } else {
-      message.channel.send(textResponse);
-    }    
+
+    let embed = new MessageEmbed()
+      .setTitle(FAQ[contentArgs].tittle)
+      .setDescription(readTextFile(FAQ[contentArgs].description));
+    message.channel.send(embed);
   }
 };
