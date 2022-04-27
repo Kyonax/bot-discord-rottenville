bc3e69e44d434b5e5e9b512528f17fab3f2bee4f
Kyonax <kyonax25@gmail.com>
2022-04-26 19:23:08 -0500

    locale: New Data upload FAQ
    FAQ questions and files for the command

--- src/command/guild/info.js
+++ src/command/guild/info.js
@@ -11,20 +11,13 @@ const BaseCommand = require("../../utils/structure/BaseCommand");
 
 function readTextFile(file)
 {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+  return data
 }
 
 //Exportación del Comando news
