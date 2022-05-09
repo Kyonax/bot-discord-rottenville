b1316358c2588dcb3d46c7d7d31f6b262fad72b5
Kyonax <kyonax25@gmail.com>
2022-05-09 16:11:08 -0500

    update: New week Rotten Events
    Weekly data about members, are updated for the new week

---
+++ src/command/guild/whitelist.js
@@ -0,0 +1,46 @@
+//Importación especifica de Metodos - MessageEmbed - nonecolor Color - putEmoji Function
+const { MessageEmbed } = require("discord.js");
+const { noneColor } = require("../../../database/utils/color/color.json");
+const { putEmoji } = require("../../utils/misc/functions");
+const { synchronous } = require("../../../database/utils/emojis/emojis.json");
+const Whitelist = require("../../../database/misc/Whitelist.json")
+//Importación de el cuerpo de Comandos e importación de Conexión Base de Datos
+const BaseCommand = require("../../utils/structure/BaseCommand");
+//Exportación del Comando Whitelist
+module.exports = class WhitelistCommand extends BaseCommand {
+  constructor() {
+    super(
+      "whitelist",
+      ["wl", "lista"],
+      "Whitelist for the FREE MINT.",
+      "whitelist`",
+      "_***Everyone***_",
+      "guild"
+    );
+  }
+  async run(bot, message, args) {
+    if (message.guild.id != "894634118267146272") return;
+    //Eliminacion del mensaje enviado por el usuario al ejecutar el Comando
+    message.delete().catch((O_o) => {});
+    //Creación de Mensaje Embed
+    let embed = new MessageEmbed().setColor(noneColor);
+    //Emoji from Map
+    let msg = null;
+    let wallet = args[0]
+    const emoji = synchronous.emojiID[0].afirmado;
+    console.log(wallet)
+    
+
+    
+    let bodyNet = null;
+    let url = null;
+    let mint = null;
+    let holder = null;
+    //Creación de Objetos
+    const err = new Error();
+    //Solicitando Json
+    
+
+
+  }
+};
