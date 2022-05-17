feb3d2e25bcbea78d0c9b3b4f2190da74327a1e0
Kyonax <kyonax25@gmail.com>
2022-05-17 15:09:08 -0500

    API

--- src/command/owner/hash.js
+++ src/command/owner/hash.js
@@ -15,103 +15,123 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const theblockchainapi = require("theblockchainapi");
 
 module.exports = class HashCommand extends BaseCommand {
-    constructor() {
-        super(
-            "hash",
-            ["fetchhash"],
-            "Fetch all the RottenVille Holders",
-            "hash`",
-            "_***Owner***_",
-            "miscellaneous"
-        );
-    }
+  constructor() {
+    super(
+      "hash",
+      ["fetchhash"],
+      "Fetch all the RottenVille Holders",
+      "hash`",
+      "_***Owner***_",
+      "miscellaneous"
+    );
+  }
 
-    async run(bot, message, args) {
-        if (message.guild.id != "894634118267146272") return;
-        //Eliminacion del mensaje enviado por el usuario al ejecutar el Comando
-        message.delete().catch((O_o) => { });
-        const CandyData = CandyRottenVille.alpha_rottens;
-        let bodyNet = null;
-        let url = null;
-        let mint = null;
-        let holder = null;
-        //Creación de Objetos
-        const err = new Error();
-        //Solicitando Json
-        for (let i = 0, len = CandyData.length; i < len; i++) {
-            CandyData.forEach(async (data) => {
-                if (data.name == "Rotten Ville #" + i) {
-                    let defaultClient = theblockchainapi.ApiClient.instance;
+  async run(bot, message, args) {
+    if (message.guild.id != "894634118267146272") return;
+    //Eliminacion del mensaje enviado por el usuario al ejecutar el Comando
+    message.delete().catch((O_o) => {});
+    const CandyData = CandyRottenVille.alpha_rottens;
+    let bodyNet = null;
+    let url = null;
+    let mint = null;
+    let holder = null, iw = 0;
+    //Creación de Objetos
+    const err = new Error();
+    //Solicitando Json
+    try {
+      
 
-                    // Get a free API Key Pair here: https://dashboard.blockchainapi.com/api-keys
+      function fetchingHash() {
+        setTimeout(async function () {
+          if (iw < CandyData.length) {
+              let data = CandyData[iw];
+                          
+            if (data.name == "Rotten Ville #" + i) {
+                let defaultClient = theblockchainapi.ApiClient.instance;
 
-                    let APIKeyID = defaultClient.authentications["APIKeyID"];
-                    APIKeyID.apiKey = "Jg7gGpZ7E0qjtJ2";
+                // Get a free API Key Pair here: https://dashboard.blockchainapi.com/api-keys
 
-                    let APISecretKey = defaultClient.authentications["APISecretKey"];
-                    APISecretKey.apiKey = "Pr9iSo0eIoProXA";
+                let APIKeyID = defaultClient.authentications["APIKeyID"];
+                APIKeyID.apiKey = "Jg7gGpZ7E0qjtJ2";
 
-                    let apiInstance = new theblockchainapi.SolanaNFTApi();
+                let APISecretKey = defaultClient.authentications["APISecretKey"];
+                APISecretKey.apiKey = "Pr9iSo0eIoProXA";
 
-                    let network = "mainnet-beta"; // String | The network ID (devnet, mainnet-beta)
-                    let mintAddress = data.token; // String | The mint address of the NFT
+                let apiInstance = new theblockchainapi.SolanaNFTApi();
 
-                    const _body = await apiInstance
-                        .solanaGetNFT(network, mintAddress)
-                        .then(
-                            (data) => {
-                                console.log("API called successfully.");
-                                return data;
-                            },
-                            (error) => {
-                                console.error(error);
-                                return null;
-                            }
-                        );
+                let network = "mainnet-beta"; // String | The network ID (devnet, mainnet-beta)
+                let mintAddress = data.token; // String | The mint address of the NFT
 
-                    let view_nft_url =
-                        "https://explorer.solana.com/address/" +
-                        mintAddress +
-                        "?cluster=" +
-                        network;
-                    console.log("View the NFT: " + view_nft_url);
-
-                    const result = await apiInstance
-                        .solanaGetNFTOwner(network, mintAddress)
-                        .then(
-                            (data) => {
-                                console.log("API called successfully.");
-                                return data;
-                            },
-                            (error) => {
-                                console.error(error);
-                                return null;
-                            }
-                        );
-
-                    let nft_owner = result["nft_owner"];
-                    url = _body.data.uri;
-                    mint = data.token;
-
-                    AlphaNFTs.alpha_rottens.push({
-                        "name": data.name,
-                        "symbol": data.symbol,
-                        "uri": url,
-                        "token": data.token,
-                        "holder": nft_owner,
-                        "explorer_url": _body.explorer_url,
-                        "edition": data.edition                                                
-                    })
-
-                    await fs.writeFileSync(
-                        "./database/utils/adds/AlphaNFTs.json",
-                        JSON.stringify(AlphaNFTs),
-                        (err) => {
-                          if (err) console.log(err);
+                const _body = await apiInstance
+                    .solanaGetNFT(network, mintAddress)
+                    .then(
+                        (data) => {
+                            console.log("API called successfully.");
+                            return data;
+                        },
+                        (error) => {
+                            console.error(error);
+                            return null;
                         }
-                      );
-                }
-            });
-        }
+                    );
+
+                let view_nft_url =
+                    "https://explorer.solana.com/address/" +
+                    mintAddress +
+                    "?cluster=" +
+                    network;
+                console.log("View the NFT: " + view_nft_url);
+
+                const result = await apiInstance
+                    .solanaGetNFTOwner(network, mintAddress)
+                    .then(
+                        (data) => {
+                            console.log("API called successfully.");
+                            return data;
+                        },
+                        (error) => {
+                            console.error(error);
+                            return null;
+                        }
+                    );
+
+                let nft_owner = result["nft_owner"];
+                url = _body.data.uri;
+                mint = data.token;
+
+                AlphaNFTs.alpha_rottens.push({
+                    "name": data.name,
+                    "symbol": data.symbol,
+                    "uri": url,
+                    "token": data.token,
+                    "holder": nft_owner,
+                    "explorer_url": _body.explorer_url,
+                    "edition": data.edition                                                
+                })
+
+                await fs.writeFileSync(
+                    "./database/utils/adds/AlphaNFTs.json",
+                    JSON.stringify(AlphaNFTs),
+                    (err) => {
+                      if (err) console.log(err);
+                    }
+                  );
+            }
+
+          }
+
+          iw++;
+          if (iw < CandyData.length) {
+            fetchingHash();
+          } else {
+            console.log(`[] Fetching finish`);
+          }
+        }, 1000);
+      }
+
+      fetchingHash();
+    } catch (error) {
+      console.log(error);
     }
+  }
 };
