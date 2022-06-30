8d49068b04d6bf1edb090f060b0cbcfbd881b5ea
Kyonax <kyonax25@gmail.com>
2022-06-30 12:27:54 -0500

    update: User Command
    Command Rank updated now with API

--- src/command/member/rank.js
+++ src/command/member/rank.js
@@ -42,10 +42,10 @@ module.exports = class InventaryCommand extends BaseCommand {
           ? "#ffffff"
           : member.displayHexColor)
       .setFooter(`Last Update ${data_member.rank.updated}`)
-      .addField("`🔱` `⚜️` Experience Ranking", `Your XP Data & Rankings of the Server, if you want to be one of the **Top 10 Members** being an active member in the Server.`, false)
-      .addField("Rank General", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.general + "`" + ` **XP:** ` + "`" + data_member.status.xp + "` **Level:** `" + data_member.status.level + "`", true)
-      .addField("Rank Month", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.special.month.data + "`" + ` **XP:** ` + "`" + data_member.rank.special.month.xp, true)
-      .addField("Rank Week", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.special.week.data + "`" + ` **XP:** ` + "`" + data_member.rank.special.week.xp, true)
+      .addField("`🔱` `⚜️` XP Server Ranking - " + member.displayName + "` **Level:** " + data_member.status.level, `Your XP Data & Rankings of the Server, if you want to be one **Top 10 Member** increase your activity in the Server.`, false)
+      .addField("Rank General", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.general + "`" + ` **XP:** ` + "`" + data_member.status.xp + "`", true)
+      .addField("Rank Month", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.special.month.data + "`" + ` **XP:** ` + "`" + data_member.rank.special.month.xp + "`", true)
+      .addField("Rank Week", `${putEmoji(bot, "910559141611860070")} ` + "`#" + data_member.rank.special.week.data + "`" + ` **XP:** ` + "`" + data_member.rank.special.week.xp + "`", true)
       .setTimestamp();
 
     message.channel.send(
