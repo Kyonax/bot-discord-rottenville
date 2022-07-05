59d0d1a82aadde98ec908cfa1f7c49476e3c165e
Kyonax <kyonax25@gmail.com>
2022-07-05 17:02:39 -0500

    testing: New Events for Discord
    Beta Riddle

--- src/utils/misc/DiscordEvents.js
+++ src/utils/misc/DiscordEvents.js
@@ -1,7 +1,11 @@
 module.exports = {
     betaRiddle: async function (Api, bot, message) {
-        let _Objguild = await Api.getGuild(message.guild.id), _Objmember = await Api.getMember(message.author.id);
-        console.log(_Objguild)
-        console.log(_Objmember)
+        try {
+            let _Objguild = await Api.getGuild(message.guild.id), _Objmember = await Api.getMember(message.author.id);
+            console.log(_Objguild)
+            console.log(_Objmember)
+        } catch (error) {
+            console.log(error)
+        }
     },
 };
