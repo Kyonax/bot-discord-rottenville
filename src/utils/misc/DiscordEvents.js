6bc18584b89e7bb22fcb79dd569f31a3cb296b5b
Kyonax <kyonax25@gmail.com>
2022-07-11 12:12:28 -0500

    event: Creation of Beta Riddle

--- src/utils/misc/DiscordEvents.js
+++ src/utils/misc/DiscordEvents.js
@@ -30,6 +30,7 @@ module.exports = {
                             .setThumbnail('https://cdn.discordapp.com/attachments/898963695336583169/994724270276096030/Cofre_R_U_DEAD.png')
                             .setFooter("💀 r u dead?")
                             .setTimestamp();
+                            message.author.send('k&0_[ii_')
                         message.channel.send(embed)
                     }
 
