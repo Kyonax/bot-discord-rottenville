ff87f7c879acf7089f78f09ec38b93e45cfe8134
Kyonax <kyonax25@gmail.com>
2022-07-11 08:26:56 -0500

    event: Creation of Beta Riddle

--- src/utils/misc/DiscordEvents.js
+++ src/utils/misc/DiscordEvents.js
@@ -3,7 +3,7 @@ module.exports = {
         try {
             let _Objguild = await Api.getGuild(message.guild.id), _Objmember = await Api.getMember(message.guild.id, message.author.id);
 
-            console.log(message.content.slice(0, 1))
+            console.log(message.content.slice(0, 8))
             console.log(message.content)
         } catch (error) {
             console.log(error)
