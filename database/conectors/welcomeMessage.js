832d0405c0d876ec08442a40d17cdfa8ce87b9db
Kyonax <kyonax@KYONAX-DESK>
2023-05-13 01:31:31 -0500

    Updating gm magik timers

--- database/conectors/welcomeMessage.js
+++ database/conectors/welcomeMessage.js
@@ -36,8 +36,6 @@ async function edit(firstImage, secondImage, name, username, size) {
   }
 }
 async function secondStep(inImage, name, username, size) {
-  let image = 'image Over 47,55 0,0 "'+inImage+'"';
-
   gm(backgroundNewUser)
     .gravity("East")
     .fill("#fb163b")
@@ -46,7 +44,7 @@ async function secondStep(inImage, name, username, size) {
     .fill("#fb163b")
     .font("Helvetica", 20)
     .drawText(136, 254, `${"#" + size + 1}`, "North")
-    .draw([image])
+    .draw([`image over 47,55 0,0 '${inImage}'`])
     .write(`database/multimedia/images/magik/exports/${username}.png`, function (err) {
       console.log("Done! SecondStep");
       if (err) console.log("Error Second Step!: " + err);
